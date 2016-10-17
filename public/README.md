9c17f42f5a59c854a4434bdc26cffb480fbd2946
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-10-17 08:59:39 -0500

    made readME

---
+++ public/README.md
@@ -0,0 +1,6 @@
+The Archive is a application that allows users to have a library of their movies
+that they have at home. I made this so when I am shopping for new movies I can
+search my app that I hosted on Heroku to ensure I don't buy the same movie twice.
+The app allows the user to search for new movies using an API from OMDB. Your library
+will be sorted in alphabetical order so it is easy to search while shopping. The
+technologies I used are SQL, Bootstrap, jQuery, Express, and Node.js.
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,6 +3,7 @@ console.log('scripts.js is sourced');
 $(document).ready(function(){
    console.log('doc ready JQ');
 
+
    getMovies();
 
   $('body').on('click', '#addButton', function(){
@@ -21,17 +22,37 @@ $(document).ready(function(){
     getMovies();
 }); // end $('body').on('click', '#addButton', function()
 
-  // hide and show of user screen
-  $("#welcomeUser").hide();
-  $('#logoutButton').hide();
-  $("#loginButton").click(function(){
-    console.log("login button clicked");
-  $('#logoutButton').show();
-  $("#welcomeUser").show();
-  $("#loginButton").hide();
-  $('#userName').hide();
-  $('#password').hide();
-  }); // end login button click
+// var lock = new Auth0Lock( 'qxFfdrj7POhBrAyn0daFWcsSfy8KI85O', 'jazzycordaro.auth0.com');
+// // log out url, from Auth0
+// var logOutUrl = 'https://YOURAUTH0SUBDOMAIN.auth0.com/v2/logout';
+
+
+//
+// $(document).on('click', '#login', function(){
+// if (err) {
+//         console.error( "auth error: ", err);
+//       } // end error
+//       else {
+//         // save token to localStorage
+//         localStorage.setItem( 'userToken', token );
+//         console.log( 'token:', token );
+//         // save user profile to localStorage
+//         localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+//         console.log( 'profile:', profile );
+//       } // end no error
+// }
+
+  // // hide and show of user screen
+  // $("#library").hide();
+  // $('#logoutButton').hide();
+  // $("#loginButton").click(function(){
+  //   console.log("login button clicked");
+  // $('#logoutButton').show();
+  // $("#library").show();
+  // $("#loginButton").hide();
+  // $('#userName').hide();
+  // $('#password').hide();
+  // }); // end login button click
 
   $(document).on('click', '#searchNew',  function(){
     var searchNewMovie = $('#searchTitle').val();
